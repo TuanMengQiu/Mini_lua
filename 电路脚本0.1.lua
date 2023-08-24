466f18b34d75c7fbad47113d32a4281011bc7a71
团梦秋 <89502805+TuanMengQiu@users.noreply.github.com>
2023-08-24 23:37:23 +0800

    Add files via upload

---
+++ 电路脚本0.1.lua
@@ -0,0 +1,44 @@
+--欢迎使用电路脚本，脚本于1.22.0版本迷你世界
+--团梦秋制作迷你号636968783>
+local function dianlu(event)
+
+s = 1 --道具数量<默认为1>
+
+
+local result,successNum=Backpack:addItem(event.eventobjid,701,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,706,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,1009,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,415,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,702,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,704,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,1033,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,707,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,711,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,712,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,713,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,1183,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,715,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,716,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,717,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,718,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,719,s)
+--团梦秋制作迷你号636968783>
+local result,successNum=Backpack:addItem(event.eventobjid,724,s)
+end
+ScriptSupportEvent:registerEvent([=[Game.AnyPlayer.EnterGame]=], dianlu)
